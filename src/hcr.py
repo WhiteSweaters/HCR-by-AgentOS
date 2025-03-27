5c784a22aeef8136564e14ac7df90c3cfa796017
Nuyoahwjl <3508529368@qq.com>
2025-03-27 17:04:30 +0000

    Create hcr.py

---
+++ src/hcr.py
@@ -0,0 +1,9 @@
+import sys
+import os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(current_dir)
+sys.path.insert(0, project_root)
+
+# print(project_root)
+
+from agentos.agent.agent import Agent
