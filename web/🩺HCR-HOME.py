88a8818a67acf12b810138c4f9abf4fc6b5c8999
Nuyoahwjl <3508529368@qq.com>
2025-04-25 06:54:51 +0000

    Update homepage

--- web/🩺HCR-HOME.py
+++ web/🩺HCR-HOME.py
@@ -1,19 +1,39 @@
 import streamlit as st
+import os
+from pathlib import Path
+current_dir = Path(__file__).parent
+img_dir = current_dir.parent / "img"
+# print(list(img_dir.glob("*.png")))
 
 st.set_page_config(page_title="HCR", page_icon="🩺")
 
 st.markdown(
-"""
-<div style="display: flex; justify-content: center;">
-<p align="center">
-  <img src="https://cdn.jsdelivr.net/gh/Tarikul-Islam-Anik/Animated-Fluent-Emojis@master/Emojis/Smilies/Face%20with%20Tongue.png" width="10%" />
-  <img src="https://cdn.jsdelivr.net/gh/Tarikul-Islam-Anik/Animated-Fluent-Emojis@master/Emojis/Smilies/Face%20with%20Spiral%20Eyes.png" width="10%" />
-  <img src="https://cdn.jsdelivr.net/gh/Tarikul-Islam-Anik/Animated-Fluent-Emojis@master/Emojis/Smilies/Relieved%20Face.png" width="10%" />
-  <img src="https://cdn.jsdelivr.net/gh/Tarikul-Islam-Anik/Animated-Fluent-Emojis@master/Emojis/Smilies/Astonished%20Face.png" width="10%" />
-  <img src="https://cdn.jsdelivr.net/gh/Tarikul-Islam-Anik/Animated-Fluent-Emojis@master/Emojis/Smilies/Beaming%20Face%20with%20Smiling%20Eyes.png" width="10%" />
-</p>	
-</div>
-""", unsafe_allow_html=True)
+    f"""
+    <div style="display: flex; justify-content: center;">
+    <p align="center">
+      <img src="{img_dir / '1.png'}" width="10%" />
+      <img src="{img_dir / '2.png'}" width="10%" />
+      <img src="{img_dir / '3.png'}" width="10%" />
+      <img src="{img_dir / '4.png'}" width="10%" />
+      <img src="{img_dir / '5.png'}" width="10%" />
+    </p>	
+    </div>
+    """, 
+    unsafe_allow_html=True
+)
+
+# st.markdown(
+# """
+# <div style="display: flex; justify-content: center;">
+# <p align="center">
+#   <img src="https://cdn.jsdelivr.net/gh/Tarikul-Islam-Anik/Animated-Fluent-Emojis@master/Emojis/Smilies/Face%20with%20Tongue.png" width="10%" />
+#   <img src="https://cdn.jsdelivr.net/gh/Tarikul-Islam-Anik/Animated-Fluent-Emojis@master/Emojis/Smilies/Face%20with%20Spiral%20Eyes.png" width="10%" />
+#   <img src="https://cdn.jsdelivr.net/gh/Tarikul-Islam-Anik/Animated-Fluent-Emojis@master/Emojis/Smilies/Relieved%20Face.png" width="10%" />
+#   <img src="https://cdn.jsdelivr.net/gh/Tarikul-Islam-Anik/Animated-Fluent-Emojis@master/Emojis/Smilies/Astonished%20Face.png" width="10%" />
+#   <img src="https://cdn.jsdelivr.net/gh/Tarikul-Islam-Anik/Animated-Fluent-Emojis@master/Emojis/Smilies/Beaming%20Face%20with%20Smiling%20Eyes.png" width="10%" />
+# </p>	
+# </div>
+# """, unsafe_allow_html=True)
 
 st.markdown("-------------")
 
